76541793a882057daa09cd90a47522482ae09e6e
Igor Venturelli <igor.oliveira@mult-e.com.br>
2018-07-12 09:45:42 -0300

    Finishing

--- apt.py
+++ apt.py
@@ -43,25 +43,40 @@ def eh_feriado():
 @sched.scheduled_job('cron', id='job-entrada', day_of_week='mon-fri', hour='08', minute='00')
 def aponta_entrada():
     if not eh_feriado():
+        aponta('start')
         logging.info('>>> Entrada')
 
 
 @sched.scheduled_job('cron', id='job-inicio-almoco', day_of_week='mon-fri', hour='12', minute='00')
 def aponta_inicio_almoco():
     if not eh_feriado():
+        aponta('finish')
         logging.info('>>> Início almoço')
 
 
 @sched.scheduled_job('cron', id='job-fim-almoco', day_of_week='mon-fri', hour='13', minute='00')
 def aponta_fim_almoco():
     if not eh_feriado():
+        aponta('start')
         logging.info('<<< Volta do almoço')
 
 
 @sched.scheduled_job('cron', id='saida', day_of_week='mon-fri', hour='17', minute='00')
 def aponta_saida():
     if not eh_feriado():
+        aponta('finish')
         logging.info('<<< Saída')
+
+
+def aponta(entrada_ou_saida):
+    headers = {'auth': os.environ['APT_AUTH'],
+               'Content-Type':'application/json'}
+    body = {'lat': '-23.2279589',
+            'lng': '-45.8572695',
+            'address': 'Av. Brg. Faria Lima, 601'}
+    response = requests.post(os.environ['APT_URL'] + entrada_ou_saida, headers=headers, json=body)
+    print(response.status_code)
+    print(response.text)
 
 
 sched.start()
